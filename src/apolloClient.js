325fd2186d13567f5b5d7735da3c95a5fa45fdcb
Nicole Numberg <a2021116310@estudiantes.upsa.edu.bo>
2024-06-01 21:42:21 -0400

    REMOVING UNNECESSARY COMMENTS

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export function createApolloClient() {
     return new ApolloClient({
-        uri: 'http://localhost:4000/graphql', // Replace this with your GraphQL endpoint
+        uri: 'http://localhost:4000/graphql',
         cache: new InMemoryCache(),
     });
 }
